4f9722c81959d67c5e3db93c3789c5f840466177
nace <tthignac@gmail.com>
2024-03-18 23:50:15 +0100

    postgres installed in docker

---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+  """ test the calc module"""
+  def test_add_number(self):
+    res = calc.add(3, 8)
+
+    self.assertEqual(res, 11)
+
+  def test_subtract_number(self):
+    res = calc.subtract(10, 15)
+
+    self.assertEqual(res, -5)
