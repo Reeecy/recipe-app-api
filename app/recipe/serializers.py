d3ec4422464b4bf031bcc9a714fe5bd32aedbc28
nace <tthignac@gmail.com>
2024-03-20 21:46:12 +0100

    nested create and update tag implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,12 +3,52 @@ from rest_framework import serializers
 from core.models import Recipe, Tag
 
 
+class TagSerializer(serializers.ModelSerializer):
+    # serializer for tag objects
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     # serializer for recipes
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def _get_or_create_tags(self, tags, recipe):
+        # handle getting or creating tags as needed
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        # create a recipe.
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+        return recipe
+
+    def update(self, instance, validated_date):
+        # update a recipe
+        tags = validated_date.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_date.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -16,11 +56,3 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
         read_only_fields = RecipeSerializer.Meta.read_only_fields
-
-
-class TagSerializer(serializers.ModelSerializer):
-    # serializer for tag objects
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
